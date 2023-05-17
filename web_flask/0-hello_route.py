318aa3c78e137560efa4b056db6a75c7c57a7aea
Jabez-abija2399 <jabeznew2399@gmail.com>
2023-05-17 17:46:59 +0000

    port changed

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/airbnb-onepage/', methods=['GET'])
 def display_hello():
     return "Hello HBNB!"
 
